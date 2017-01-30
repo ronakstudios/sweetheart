85f8a925aed9d76bfec1154e8bbded15204f7206
ronakstudios <snozey19@gmail.com>
2017-01-29 23:22:44 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,17 @@
+(function($){
+  $(function(){
+ $('.collapsible').collapsible({
+      accordion : false 
+    });
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+$('.modal-trigger').leanModal();
+$('.carousel.carousel-slider').carousel({full_width: true});
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+var audio = new Audio('z.mp3');
+audio.play();

36f5b089fcb32771bf1622fb62da642707f10b7e
ronakstudios <snozey19@gmail.com>
2017-02-02 17:24:09 -0500

    Create init2.js

---
+++ js/init2.js
@@ -0,0 +1,22 @@
+ lines (30 sloc)  1.17 KB
+(function($){
+  $(function(){
+ $('.collapsible').collapsible({
+      accordion : false 
+    });
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+$('.modal-trigger').leanModal();
+       $(document).ready(function(){
+      $('.slider').slider({height:600});
+    });
+$('.carousel.carousel-slider').carousel({full_width: true});
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+var audio = new Audio('z.mp3');
+audio.play();
+function h(){ Materialize.toast('Stultify - Cause to lose enthusiasm and initiative, especially as a result of a tedious or restrictive routine', 4000) }

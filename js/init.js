b30c7772d69625d3c18003639ee82906613977f5
ronakstudios <snozey19@gmail.com>
2017-02-02 17:26:27 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -18,17 +18,5 @@ $('.datepicker').pickadate({
 })(jQuery); // end of jQuery name space
 var audio = new Audio('z.mp3');
 audio.play();
-
-    $(function(){
-      $(".sss").typed({
-        strings: ["Oozing with fun for the entire family.", "Catering toward birthdays, special events, and large parties.","A candy wonderland and sugary paradise.","Only $25 per child for a full day of play.","Book reservations in advance."],
-        typeSpeed: 0,
-	loop: true,
-	backDelay: 2000,
-	showCursor: false
-      });
-    });
-
-
 function h(){ Materialize.toast('Stultify - Cause to lose enthusiasm and initiative, especially as a result of a tedious or restrictive routine', 4000) }
 

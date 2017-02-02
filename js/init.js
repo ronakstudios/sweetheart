ff5a9d9b00d50c81f81cfb60bdd0809e241c0a85
ronakstudios <snozey19@gmail.com>
2017-02-02 17:17:24 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -20,12 +20,3 @@ var audio = new Audio('z.mp3');
 audio.play();
 function h(){ Materialize.toast('Stultify - Cause to lose enthusiasm and initiative, especially as a result of a tedious or restrictive routine', 4000) }
 
-    $(function(){
-      $(".sss").typed({
-        strings: ["Oozing with fun for the entire family.", "Catering toward birthdays, special events, and large parties.","A candy wonderland and sugary paradise.","Only $25 per child for a full day of play.","Book reservations in advance."],
-        typeSpeed: 0,
-	loop: true,
-	backDelay: 2000,
-	showCursor: false
-      });
-    });
